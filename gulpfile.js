2eef6fcf1a28d647d7f6efe740a55c58f76c76d7
daichangxin <daichangxin@gmail.com>
2019-10-03 10:40:29 +0800

    init build env

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+const gulp = require('gulp');
+const rollup = require('rollup');
+const node_resolve = require('rollup-plugin-node-resolve');
+const typescript = require('rollup-plugin-typescript2');
+const commonjs = require('rollup-plugin-commonjs');
+const buildins = require('rollup-plugin-node-builtins');
+
+gulp.task('build', async () => {
+    return rollup.rollup({
+        input: './src/index.ts',
+        treeshake: true,
+        plugins: [
+            buildins(),
+            node_resolve(),
+            commonjs(),
+            typescript({
+                check: false,
+                tsconfigOverride: {
+                    compilerOptions: {
+                        removeComments: true
+                    }
+                },
+                include: /.*(.ts)$/
+            }),
+        ],
+        onwarn: function (warning) {
+            if (warning.code === 'THIS_IS_UNDEFINED') {
+                return;
+            }
+            console.warn(warning.message);
+        }
+    }).then(bundle => {
+        return bundle.write({
+            file: './bin/index.js',
+            format: 'iife',
+            name: 'paw',
+        })
+    });
+});
+
+gulp.task('watch', () => {
+    gulp.watch('./src/**/*.ts', gulp.series(['build']));
+});
+
+gulp.task('default', gulp.series(['build', 'watch']));
